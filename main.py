25309552f4032c1dc4c62f2905e177c7f7026561
User <slavachekanof@yandex.ru>
2022-07-12 22:46:34 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+__author__ = "Чеканов Вячеслав Александрович"
+name = ("Slava")
+age = 23
+print("My name is", name,".I'm", age, "years old.")
+
+#TODO
+print(age - 18)
+
+name = ("Vasya")
+age1 = 18
+print("My name is ", name, ". I'm ", age1, " years old.")
+print(age1 + 2 )
+print(age1 + 5)
+
+#TODO 2
+print("Please enter 2 variable")
+a = input("Variable 1: ")
+b = input("Variable 2: ")
+
+print("Variable 1: ", a, "Variable 2: ", b)
+
